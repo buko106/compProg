8b89e852abaefba592085d7aec2447c7a0f88fb8
buko106 <buko1062000@yahoo.co.jp>
2017-09-30 01:16:11 +0900

    define macros

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
+#define rep(i,n) for(int i=0;i< (n);++i)
+#define REP(i,n) for(int i=1;i<=(n);++i)
+#define ALL(a) (a).begin(),(a).end()
+
 int main() {
     std::ios::sync_with_stdio(false);
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
