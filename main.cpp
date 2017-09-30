5b2c016875153af0011bbe3dca3881f49acc757f
buko106 <buko1062000@yahoo.co.jp>
2017-09-30 11:24:08 +0900

    added std::vector and std::pair aliases

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@
 using LLI = long long int;
 LLI GCD(LLI a,LLI b){ if(a<b) return GCD(b,a); else if(a%b==0) return b; else return GCD(b,a%b); }
 LLI LCM(LLI a,LLI b){ return (a/GCD(a,b))*b; }
+using VI  = std::vector<LLI>;
+using PI  = std::pair<LLI,LLI>;
+using VVI = std::vector<VI>;
+using VPI = std::vector<PI>;
 
 int main() {
     std::ios::sync_with_stdio(false);
