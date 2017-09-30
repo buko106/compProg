7767dcc91b3cd24ef2763fc307d062d00fe5b785
buko106 <buko1062000@yahoo.co.jp>
2017-09-30 15:08:27 +0900

    added for_uppercase

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #define rep(i,n) for(int i=0;i< (n);++i)
 #define REP(i,n) for(int i=1;i<=(n);++i)
 #define for_lowercase(c) for(int c='a';c<='z';++c)
+#define for_uppercase(c) for(int c='A';c<='Z';++c)
 #define ALL(a) (a).begin(),(a).end()
 using LLI = long long int;
 LLI GCD(LLI a,LLI b){ if(a<b) return GCD(b,a); else if(a%b==0) return b; else return GCD(b,a%b); }
