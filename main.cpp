4145ac71ba79d870ad1cdb30942298c9820c245a
buko106 <buko1062000@yahoo.co.jp>
2017-09-30 10:54:36 +0900

    #define -> using

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #define rep(i,n) for(int i=0;i< (n);++i)
 #define REP(i,n) for(int i=1;i<=(n);++i)
 #define ALL(a) (a).begin(),(a).end()
-#define LLI long long int
+using LLI = long long int;
 LLI GCD(LLI a,LLI b){ if(a<b) return GCD(b,a); else if(a%b==0) return b; else return GCD(b,a%b); }
 LLI LCM(LLI a,LLI b){ return (a/GCD(a,b))*b; }
 
