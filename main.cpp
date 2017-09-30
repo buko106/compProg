a0f92cdb37fe99376dba5f1270a0e910455d02b3
buko106 <buko1062000@yahoo.co.jp>
2017-09-30 13:27:45 +0900

    added for_lowercase is_odd is_even

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 
 #define rep(i,n) for(int i=0;i< (n);++i)
 #define REP(i,n) for(int i=1;i<=(n);++i)
+#define for_lowercase(c) for(int c='a';c<='z';++c)
 #define ALL(a) (a).begin(),(a).end()
 using LLI = long long int;
 LLI GCD(LLI a,LLI b){ if(a<b) return GCD(b,a); else if(a%b==0) return b; else return GCD(b,a%b); }
@@ -13,6 +14,8 @@ using VI  = std::vector<LLI>;
 using PI  = std::pair<LLI,LLI>;
 using VVI = std::vector<VI>;
 using VPI = std::vector<PI>;
+template<class T> bool is_odd (T x){ return   x%2 ; }
+template<class T> bool is_even(T x){ return !(x%2); }
 
 int main() {
     std::ios::sync_with_stdio(false);
